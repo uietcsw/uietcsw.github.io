f9c4df6f9656c90b11b371ec0ca527127cf2fc06
shubh <shubhkirti.work@gmail.com>
2017-06-22 21:43:03 +0530

    Major Update

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,10 +2,6 @@
   Loading Function
 */
 $(window).load(function() {
-  $(".back-1").delay(1000).fadeTo("slow",1);
-  $(".back-2").delay(2000).fadeTo("slow",1);
-  $(".back-3").delay(3000).fadeTo("slow",1);
-  $(".back-4").delay(4000).fadeTo("slow",1);
-  $(".back-5").delay(5000).fadeTo("slow",1);
-  $(".loader").delay(6500).fadeOut("slow");
+  $(".loader").fadeIn("slow");  
+  $(".loader").delay(500).fadeOut("slow");
 })
